5d385c6ae9dfaf797aea83ec8ea65e2e850a55da
dhwaj2209 <dhwaj2209@gmail.com>
2025-06-01 07:39:46 +0530

    mysql in python  demo added

---
+++ database-connectivity-demo/mysql-demo.py
@@ -0,0 +1,112 @@
+# MySql in Python - CRUD Operations
+# create connection from MYSQL in Pyton
+import mysql.connector
+from mysql.connector import connection
+con = mysql.connector.connect(host="localhost",
+                              user="user1",
+                              password="@user112345")
+print("Connection is created")
+con.close()
+
+con = connection.MySQLConnection(host="localhost",
+                              user="user1",
+                              password="@user112345")
+print("Connection1 is created")
+con.close()
+
+# create new table in MYSQL database in Python
+
+from mysql.connector import connection
+dic = {
+"host":"localhost",
+"user":"user1",
+"password":"@user112345",
+"database":"employee"
+}
+query = """
+CREATE TABLE users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100)
+)
+"""
+try:
+  with connection.MySQLConnection(**dic) as con:
+    with con.cursor() as cursor:
+      cursor.execute(query)
+      con.commit()
+except mysql.connector.Error as err:
+  print("Error message:", err.msg)  # error message
+
+
+# fetch many
+
+#result = cur.fetchmany(size = 2)
+
+# select records with where and order by clause
+from mysql.connector import connection
+dic = {
+"host":"localhost",
+"user":"user1",
+"password":"@user112345",
+"database":"employee"
+}
+query = """
+select * from users where id > 2 order by name;
+"""
+try:
+    with connection.MySQLConnection(**dic) as conn:
+        cur = conn.cursor()
+        cur.execute(query)
+        result = cur.fetchall()
+        for f in result:
+            print(f)
+        #conn.commit()
+except mysql.connector.Error as err:
+    print("Connection is not established due to some error!")
+
+# update records on MYSQL in Pyton
+
+from mysql.connector import connection
+dic = {
+"host":"localhost",
+"user":"user1",
+"password":"@user112345",
+"database":"employee"
+}
+query = """
+update users 
+set name = 'Mike' 
+where id = 1;
+"""
+try:
+    with connection.MySQLConnection(**dic) as conn:
+        cur = conn.cursor()
+        cur.execute(query)
+        conn.commit()
+
+
+except mysql.connector.Error as err:
+    print("Connection is not established due to some error!")
+
+# delete records from MYSQL in Pyton
+
+from mysql.connector import connection
+dic = {
+"host":"localhost",
+"user":"user1",
+"password":"@user112345",
+"database":"employee"
+}
+query = """
+delete from users  `
+where id > 3 and name = 'Adam';
+"""
+try:
+    with connection.MySQLConnection(**dic) as conn:
+        cur = conn.cursor()
+        cur.execute(query)
+        conn.commit()
+except mysql.connector.Error as err:
+    print("Connection is not established due to some error!")
+
+
